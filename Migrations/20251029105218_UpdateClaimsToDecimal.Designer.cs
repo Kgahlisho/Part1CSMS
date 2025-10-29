1c330c946e44c4245c247bdb68d4c532d4a63e06
RC_Student_Lab <RC_Student_Lab@LabVM1846780>
2025-10-29 15:00:48 +0200

    added new tables on the layout to imporve the flow of the website .where every page has different statuses of the claims from the coordinator to the manager

--- Migrations/20251029105218_UpdateClaimsToDecimal.Designer.cs
+++ Migrations/20251029105218_UpdateClaimsToDecimal.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Part1ex.Data;
 
@@ -11,9 +12,11 @@ using Part1ex.Data;
 namespace Part1ex.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251029105218_UpdateClaimsToDecimal")]
+    partial class UpdateClaimsToDecimal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
